771b0faf73b49fd2145ccc132766fdd121fe8e67
Maximilian Algehed <m.algehed@gmail.com>
2018-03-10 15:59:25 +0100

    boooooooooooooooring

--- src/L/TypeCheck.hs
+++ src/L/TypeCheck.hs
@@ -9,7 +9,7 @@ import L.Abs
 
 data TCState = TCS { constructorTypes :: M.Map UIdent (Type, [Type])
                    , functionTypes    :: M.Map LIdent (Type, [Type])
-                   , variableTypes    :: M.Map LIdent Type }
+                   , variableTypes    :: [M.Map LIdent Type] }
 
 type TC a = StateT TCState (Either String) a
 
@@ -18,16 +18,56 @@ lookup f v = do
   ctx <- gets f
   maybe typeError return (M.lookup v ctx)
 
+lookupVar :: LIdent -> TC Type
+lookupVar v = do
+  ctx <- gets variableTypes
+  case [ t | Just t <- M.lookup v <$> ctx ] of
+    []    -> typeError
+    (t:_) -> return t
+
+push :: TC ()
+push = modify $ \s -> s { variableTypes = M.empty : variableTypes s }
+
+pop :: TC ()
+pop = modify $ \s -> s { variableTypes = drop 1 (variableTypes s) }
+
+introduce :: LIdent -> Type -> TC ()
+introduce v t = do
+  ctx <- gets variableTypes
+  case ctx of
+    []     -> typeError
+    (m:ms) -> case M.lookup v m of
+      Nothing -> modify $ \s -> s { variableTypes = M.insert v t m : ms }
+      Just _  -> typeError
+
 typeError :: TC a
 typeError = fail "Type error"
 
 class TypeCheckable a where
   typeCheck :: Type -> a -> TC ()
 
+instance TypeCheckable Body where
+  typeCheck t b = case b of
+    BCase x as -> do
+      xt <- lookupVar x
+      sequence_ [ push >> typeCheck xt p >> typeCheck t e >> pop
+                | A p e <- as ]
+    BExpr e -> typeCheck t e
+
+-- Binds variables in the current context
+instance TypeCheckable Pat where
+  typeCheck t p = case p of
+    PVar v    -> introduce v t
+    PCon c ps -> do
+      (rt, argst) <- lookup constructorTypes c
+      unless (rt == t) typeError
+      unless (length ps == length argst) typeError
+      zipWithM_ typeCheck argst ps 
+
 instance TypeCheckable Expr where
   typeCheck t e = case e of
     EVar v     -> do
-      vt <- lookup variableTypes v
+      vt <- lookupVar v
       unless (t == vt) typeError
     ECon c     -> do
       (rt, argst) <- lookup constructorTypes c
